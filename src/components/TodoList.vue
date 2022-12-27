f1f992d882d3d11ddf175bcd129c28ddb962c785
moovyasha <kuzmin786.vk@gmail.com>
2022-12-27 15:09:43 +0300

    5

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,8 +5,7 @@
                 v-for="(elOfList, i) of taskList1" 
                 v-bind:elOfList = "elOfList"
                 v-bind:index ="i"
-                v-on:removeItem="removeLi"
-            /> <!-- v-for позволяет  отрисовать массив + пробег по массиву (как цикл for). elOfList - ссылка на элемент массива, taskList1 - массив. -->
+                v-on:removeItem="removeLi"/> <!-- v-for позволяет  отрисовать массив + пробег по массиву (как цикл for). elOfList - ссылка на элемент массива, taskList1 - массив. -->
             <!-- v-on служит для обработки событий v-on:click, change и др, можно назначать свои, но тогда необходимо указать их в methods. -->
         </ul>
     </div>
@@ -15,7 +14,7 @@
 <script>
 import TodoItem from '@/components/TodoItem.vue'
 export default {
-    props: ['taskList1'], //здесь taskList1 свойство, которое передаем в главный файл App.vue в строку v-bind:taskList1 = "taskList2", где taskList1 принимает значения из массива taskList2
+    props: ['taskList1'], //здесь taskList1 свойство, которое передаем из главного файла App.vue в строке v-bind:taskList1 = "taskList2", где taskList1 принимает значения из массива taskList2
     components: {
         TodoItem
     },
